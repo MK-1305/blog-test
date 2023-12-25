f32808c27034c7b746b3435f64fa7a66b1f5f6c8
kazukimatsuura33@gmail.com <kazukimatsuura33@gmail.com>
2023-12-25 11:15:53 +0900

    INITIAL COMMIT

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>&copy; blog-test.</p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>blog-test</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>
+        <div class="header-box">
+            <p class="header-text">blog</p>
+        </div>
+    </header>
+</body>
